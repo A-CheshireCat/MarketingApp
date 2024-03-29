e789c0ca43d3e5bbe943f1c10c00579707b195cb
alexandra <123159015+A-CheshireCat@users.noreply.github.com>
2024-03-29 10:39:42 +0200

    Initial project commit

---
+++ MarketingApp/MarketingApp/MarketingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketingAppApp.swift
+//  MarketingApp
+//
+//  Created by Alexandra on 29.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MarketingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
